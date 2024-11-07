2edaa84731443fabfe0da3d9f4e1fc119c5c269f
Liliia Parashchak <liliia.parashchak@gmail.com>
2024-11-07 03:34:49 +0200

    Complete grammar rules

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ fn main() {
     let element_parse = ChemParser::parse(Rule::element, "Al");
     println!("{:?}", element_parse);
 
+    let formula_parse = ChemParser::parse(Rule::formula, "H2O");
+    println!("{:?}", formula_parse);
+
+    let equation_parse = ChemParser::parse(Rule::equation, "2H + O2 -> 2H2O");
+    println!("{:?}", equation_parse);
+
     let periodic_table = PeriodicTable::from_csv("./data/elements.csv")
         .expect("Failed to load periodic table");
 
