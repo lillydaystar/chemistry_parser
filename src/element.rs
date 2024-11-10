3df03c66447f81874685436288f5bc32f408b024
Liliia Parashchak <liliia.parashchak@gmail.com>
2024-11-11 00:42:06 +0200

    Use Serde to parse periodic table

--- src/element.rs
+++ src/element.rs
@@ -1,9 +1,10 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::Display;
-use std::fs::File;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Element {
     pub name: String,
     pub symbol: String,
@@ -11,8 +12,8 @@ pub struct Element {
     pub atomic_mass: f64,
     pub density: f64,
     pub group: Option<u8>,
-    pub melting_point: Option<f64>,
-    pub boiling_point: Option<f64>,
+    pub melting_point: Option<Value>,
+    pub boiling_point: Option<Value>,
 }
 
 impl Display for Element {
@@ -122,21 +123,12 @@ pub struct PeriodicTable {
 impl PeriodicTable {
     pub fn from_csv(path: &str) -> Result<Self, Box<dyn Error>> {
         let mut elements = HashMap::new();
-        let file = File::open(path)?;
-        let mut rdr = csv::Reader::from_reader(file);
+        let mut rdr = csv::ReaderBuilder::new()
+            .has_headers(true)
+            .from_path(path)?;
 
-        for result in rdr.records() {
-            let record = result?;
-            let element = Element {
-                name: record[0].to_string(),
-                symbol: record[1].to_string(),
-                atomic_number: record[2].parse()?,
-                atomic_mass: record[3].parse()?,
-                density: record[4].parse()?,
-                group: record[5].parse().ok(),
-                melting_point: record[6].parse().ok(),
-                boiling_point: record[7].parse().ok(),
-            };
+        for result in rdr.deserialize() {
+            let element: Element = result?;
             elements.insert(element.symbol.clone(), element);
         }
 
